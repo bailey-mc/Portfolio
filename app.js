6a732060a5390e89f00935979fa78ef30f8044ba
Bailey McDonald <bails@Baileys-MacBook-Pro.local>
2022-11-02 09:11:57 -0500

    resume images inserted, hover works, broadly styled

--- app.js
+++ app.js
@@ -45,16 +45,90 @@ $('.previous').on('click', () => {
 
 //========HOVER IMAGES RESUME=============//
 
+//=============GA STUDENT ================//
 const $hoverImage = $('#hover-image1').hide(0)
 
 
 $('#resume-item1').hover(
     () => {
 
-        $hoverImage.show(3000).hide(3000)
+        $hoverImage.show(5000)
+        // .hide(3000)
+        // $hoverImage.hide(0)
+       
+
+        
+        }
+) 
+
+
+
+//============PRESCHOOL TEACHER=============//
+const $hoverImage2 = $('#hover-image2').hide(0)
+
+
+$('#resume-item2').hover(
+    () => {
+
+        $hoverImage2.show(5000)
+        // .hide(3000)
+        // $hoverImage2.hide(0)
+       
+        
+        }
+) 
+
+//================FARMER MARKET=============//
+
+const $hoverImage3 = $('#hover-image3').hide(0)
+
+
+$('#resume-item3').hover(
+    () => {
+
+        $hoverImage3.show(3000)
+        // .hide(3000)
+        // $hoverImage3.hide(0)
+       
+
+        
+        }
+) 
+
+
+//==============WRITING TUTOR==============//
+
+
+const $hoverImage4 = $('#hover-image4').hide(0)
+
+
+$('#resume-item4').hover(
+    () => {
+
+        $hoverImage4.show(3000)
+        // .hide(3000)
+        // $hoverImage4.hide(0)
         
         
         }
 ) 
+
+
+//============UT STUDENT =================//
+
+const $hoverImage5 = $('#hover-image5').hide(0)
+
+
+$('#resume-item5').hover(
+    () => {
+
+        $hoverImage5.show(3000)
+        // .hide(3000)
+        // $hoverImage5.hide(0)
+        
+        
+        }
+) 
+
 
 })
